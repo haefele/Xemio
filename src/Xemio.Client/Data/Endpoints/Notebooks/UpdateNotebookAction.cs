f79dab9a67f0498a1cad6366100467059ccacabc
Daniel Häfele <haefele@xemio.net>
2019-02-18 23:11:46 +0100

    Move UpdateNotebookAction into its own file

--- src/Xemio.Client/Data/Endpoints/Notebooks/UpdateNotebookAction.cs
+++ src/Xemio.Client/Data/Endpoints/Notebooks/UpdateNotebookAction.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Xemio.Client.Data.Endpoints.Notebooks
 {
-    public class CreateNotebookAction
+    public class UpdateNotebookAction
     {
+        public bool UpdateName { get; set; }
         public string Name { get; set; }
+        
+        public bool UpdateParentNotebookId { get; set; }
         public string ParentNotebookId { get; set; }
     }
 }
